da1078c53808f0f92612d7042bb3ec6f642d5c1f
wyylame <w1llhorn@yandex.ru>
2022-05-20 19:48:54 +0300

    добавлен таймер и доработан функционал

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
 const startContainer = document.querySelector('.start-container'),
       startBtn = startContainer.querySelector('.start-btn'),
-      cardsContainer = document.querySelector('.cards-container')
+      cardsContainer = document.querySelector('.cards-container'),
+      modal = document.querySelector('.modal'),
+      modalText = modal.querySelector('h1'),
+      modalBtn = modal.querySelector('button'),
+      timer = document.querySelector('.timer');
 
-const count = 10;
-const cards = ['1', '2', '3', '4', '5']
-const cardsPositions = []
+const count = 10,
+      cards = ['1', '2', '3', '4', '5'],
+      cardsPositions = [];
+
+let timeIntervel = '';
+
+let activeCards = 0,
+    openCards = []
+
+const time = '1'
 
 const card = (id, position) => {
   return `
@@ -19,64 +30,16 @@ const card = (id, position) => {
   `
 }
 
-const generateCards = () => {
-  cards.map(e => {
-    cardsPositions.push(e) 
-    cardsPositions.push(e)
-  })
-  cardsPositions.sort(() => Math.random() - 0.1)
-  console.log(cardsPositions)
-}
-
-const placeCards = () => {
-  cardsPositions.map((e, i) => {
-    cardsContainer.innerHTML += card(e, i)
-  })
-}
-
-const showCardsContainer = () => {
-  cardsContainer.style.visibility = 'visible'
-}
-
-const rotateCard = (target, mode) => {
-  const elems = target.childNodes,
-        childs = [].filter.call(elems, e => e.nodeName !== '#text')
-
-  let = deg1 = '', deg2 = '';
-  if (mode === 'open') {deg1 = '180', deg2 = '360'}
-  else {deg1 = '0', deg2 = '0'}
-  
-  childs[0].style.transform = `rotateY(${deg1}deg)`
-  childs[1].style.transform = `rotateY(${deg2}deg)`
-  activeCards++;
-}
-
-const getCardsForPosition = (pos1, pos2) => {
-  return [document.querySelector(`.card[data-position="${pos1}"]`),
-          document.querySelector(`.card[data-position="${pos2}"]`)]
-}
-
-const setGuessedStatus = (pos1, pos2) => {
-  const c = getCardsForPosition(pos1, pos2)
-  c[0].classList.add('guessed')
-  c[1].classList.add('guessed')
-}
-
-let activeCards = 0;
-let openCards = []
-
-const resetData = () => {
-  activeCards = 0;
-  openCards = []
-}
-
+// старт игры
 startBtn.addEventListener('click', () => {
-  startContainer.style.display = "none";
+  displayContainer(startContainer, 'none')
   generateCards()
   placeCards()
-  showCardsContainer()
+  displayContainer(cardsContainer, 'flex')
+  initTimer()
 })
 
+// нажатие на карту
 cardsContainer.addEventListener('click', (e) => {
   const openCard = e.target.parentNode.parentNode;
   if (openCard.className === 'card' && !openCard.classList.contains('guessed')) {
@@ -88,20 +51,94 @@ cardsContainer.addEventListener('click', (e) => {
   }
 })
 
-const checkAllCards = () => {
-  const allCards = document.querySelectorAll('.card')
-  return allCards.length !== [...allCards].filter(e => e.classList.contains('guessed')).length
+// обработчик для кнопки модального окна
+modalBtn.addEventListener('click', () => {
+  resetGame()
+})
+
+// очистка временных данных
+const resetData = () => {
+  activeCards = 0;
+  openCards = []
 }
 
+// генерация карт в массиве
+const generateCards = () => {
+  cards.map(e => {
+    cardsPositions.push(e) 
+    cardsPositions.push(e)
+  })
+  cardsPositions.sort(() => Math.random() - 0.1)
+}
+
+// создание карт на html странице
+const placeCards = () => {
+  cardsPositions.map((e, i) => {
+    cardsContainer.innerHTML += card(e, i)
+  })
+}
+
+// функция для изменения показа/скрытия определённых блоков
+const displayContainer = (elem, param) => {
+  elem.style.display = param
+}
+
+// развернуть карту
+const rotateCard = (target, mode) => {
+  const childs = [...target.childNodes].filter(e => e.nodeName !== '#text')
+
+  let = deg1 = mode === 'open' ? '180' : '0', 
+        deg2 = mode === 'open' ? '360' : '0';
+  
+  childs[0].style.transform = `rotateY(${deg1}deg)`
+  childs[1].style.transform = `rotateY(${deg2}deg)`
+  activeCards++;
+}
+
+// второстепенная функция, для получения позиции карты
+const getCardsForPosition = (pos1, pos2) => {
+  return [document.querySelector(`.card[data-position="${pos1}"]`),
+          document.querySelector(`.card[data-position="${pos2}"]`)]
+}
+
+//устанавливает класс guessed, который показывает что карта отгадана
+const setGuessedStatus = (pos1, pos2) => {
+  const c = getCardsForPosition(pos1, pos2)
+  c[0].classList.add('guessed')
+  c[1].classList.add('guessed')
+}
+
+// проверка на карты, открыты ли все
+const checkAllCards = () => {
+  const allCards = document.querySelectorAll('.card')
+  if (allCards.length === [...allCards].filter(e => e.classList.contains('guessed')).length) {
+    showModal('win')
+  }
+}
+
+// отображение и настройка модального окна (поражение/выигрыш)
+const showModal = (res) => {
+  let text = res === 'win' ? 'Вы выиграли' : 'Вы проиграли',
+      color = res === 'win' ? '#42df42' : '#df4242'
+
+  modalText.innerHTML = text;
+  modalText.style.color = color;
+  modalBtn.style = `color: ${color}; border-color: ${color};`;
+
+  setTimeout(() => {
+    displayContainer(modal, 'flex')
+  }, 500)
+  clearInterval(timeIntervel)
+}
+
+// обработка 
 const handleCards = () => {
   if (activeCards === 2) {
     const pos1 = openCards[0][1], pos2 = openCards[1][1];
     if (openCards[0][0] == openCards[1][0]) {
       setGuessedStatus(pos1, pos2)
       resetData()
-      if (checkAllCards()) {
-        console.log('Молодец')
-      }
+      checkAllCards()
     } else {
       setTimeout(() => {
         const c = getCardsForPosition(pos1, pos2);
@@ -111,5 +148,42 @@ const handleCards = () => {
       }, 500)
     }
   }
+}
+
+// сброс игры
+const resetGame = () => {
+  displayContainer(modal, 'none')
+  displayContainer(cardsContainer, 'none');
+  displayContainer(timer, 'none')
+  displayContainer(startContainer, 'flex');
+
+  [...document.querySelectorAll('.card')].map((e) => {
+    e.remove()
+  })
+}
+
+// инициализация таймера
+const initTimer = () => {
+  const minutes = timer.querySelector('.minutes'),
+        seconds = timer.querySelector('.seconds')
+
+  displayContainer(timer, 'flex')
+
+  let leftTime = time*60;
+
+  function updateTimer() {
+    const m = Math.floor(leftTime/60),
+          s = Math.floor(leftTime%60)
+      
+    minutes.innerHTML = m < 10 ? `0${m}` : m
+    seconds.innerHTML = s < 10 ? `0${s}` : s
+    if (leftTime === 0) {
+      clearInterval(timeIntervel)
+      showModal('lose')
+    }
+    leftTime--;
+  }
+  updateTimer();
+  timeIntervel = setInterval(updateTimer, 1000);
 }
 
